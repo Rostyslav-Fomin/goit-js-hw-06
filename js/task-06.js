d3650b8b1366b53a8baa1da9638cc42b48541d48
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-03-27 17:11:42 +0200

    corrected Home Task

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,6 @@
 const validationInput = document.querySelector('#validation-input');
 validationInput.addEventListener('blur', checkingEntered);
 function checkingEntered(event) {
-	console.log(event.currentTarget.value.length)
 	if (event.currentTarget.value.length > 6
 		|| event.currentTarget.value.length < 6) {
 		return validationInput.classList = 'invalid';
--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,5 @@ const fontSizeControlInput = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 fontSizeControlInput.addEventListener('input', changeFontSize);
 function changeFontSize(event) {
-	console.log(event.currentTarget.value);
 	return text.style.fontSize = event.currentTarget.value  + 'px';
 }
